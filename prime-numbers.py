c08be58efd0c6bde1eb90919d0dae82a0afe0408
luisc-dominguez <luis.dominguez@enroutesystems.com>
2022-05-24 16:15:48 -0500

    Finish Prime Numbers

--- prime-numbers.py
+++ prime-numbers.py
@@ -1,1 +1,11 @@
 #Prime Numbers exercise using Python
+
+number = int(input("Enter a number: "))
+
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print(number, "is not a prime number")
+            break
+    else:
+        print(number, "is a prime number")
