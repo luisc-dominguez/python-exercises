03ebffcd24ecc6f8d7e2aa280aecc05e178b9e21
luisc-dominguez <luis.dominguez@enroutesystems.com>
2022-05-24 16:07:06 -0500

    Finish Fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,13 @@
 #Fibonacci exercise using Python
+
+def fibonacci(num):
+    if (num == 0 or num == 1):
+        return num
+    else:
+        return fibonacci(num - 1) + fibonacci(num - 2)
+  
+i = input("How many Fibonacci numbers do you want to see? ")
+max_num = int(i)
+  
+for number in range(max_num):
+    print(fibonacci(number ), end = ", ")
